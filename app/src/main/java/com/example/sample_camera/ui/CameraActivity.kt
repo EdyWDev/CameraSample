35f4a2355e3de5d66ffd77e7e650e21a051f98b1
EdyWDev <edyta.wysocka.1994@gmail.com>
2022-10-28 14:52:40 +0200

    Change calling OnActivityResult method for new way -> registerForActivityResult

--- app/src/main/java/com/example/sample_camera/ui/CameraActivity.kt
+++ app/src/main/java/com/example/sample_camera/ui/CameraActivity.kt
@@ -4,12 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder.VideoSource.CAMERA
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.webkit.URLUtil
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.ActivityChooserView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,7 +24,7 @@ import java.util.jar.Manifest
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: CameraActivityBinding
-
+    private var latestTmpUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +36,9 @@ class CameraActivity : AppCompatActivity() {
 
         binding.takePhotoBTN.setOnClickListener() {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, CAMERA)
+            takeImageResult.launch(intent)
+
+
         }
 
 
@@ -60,7 +65,15 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
+    private var takeImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+          //  doSomeOperations()
+        }
+    }
+
     companion object {
         const val REQUEST_ID_MULTIPLE_PERMISSIONS = 1
     }
 }
+
